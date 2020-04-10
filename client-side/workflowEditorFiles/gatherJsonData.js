e1379efa77acf7481e7738766be7efe1d3e3fca3
lorem-ipsum1111 <shrikant.saxena.here@gmail.com>
2020-04-10 23:33:13 +0530

    some changes

--- client-side/workflowEditorFiles/gatherJsonData.js
+++ client-side/workflowEditorFiles/gatherJsonData.js
@@ -1,5 +1,6 @@
+let defaultFormData = '"[{\"type\":\"textarea\",\"required\":false,\"label\":\"Subject\",\"className\":\"form-control\",\"name\":\"textarea-1586517925776\",\"access\":false,\"subtype\":\"textarea\"},{\"type\":\"textarea\",\"required\":false,\"label\":\"Body\",\"className\":\"form-control\",\"name\":\"textarea-1586517959538\",\"access\":false,\"subtype\":\"textarea\"}]"';
+
 function fetchAndSendData(){
-    let defaultFormData = '"[{\"type\":\"textarea\",\"required\":false,\"label\":\"Subject\",\"className\":\"form-control\",\"name\":\"textarea-1586517925776\",\"access\":false,\"subtype\":\"textarea\"},{\"type\":\"textarea\",\"required\":false,\"label\":\"Body\",\"className\":\"form-control\",\"name\":\"textarea-1586517959538\",\"access\":false,\"subtype\":\"textarea\"}]"';
 
     try {
         defaultFormData = localStorage.getItem("formData");
