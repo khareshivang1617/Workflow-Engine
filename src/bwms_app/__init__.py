ca98cc7ba12d363d1a9eec98358520653ece4ffd
Sapna20 <2017csb1105@iitrpr.ac.in>
2020-04-10 23:48:56 +0530

    some functionality of system admin added

--- src/bwms_app/__init__.py
+++ src/bwms_app/__init__.py
@@ -7,26 +7,28 @@ app.config.from_object('config')
 app.config.from_pyfile('config.py')
 try:
     db_url = app.config['DATABASE_URI']
+    print(db_url)
     conn = psycopg2.connect(db_url)
     cursor = conn.cursor()
 
-    # uncomment this for checking the connection
-    # print('PostgreSQL database version:')
-    # cur.execute('SELECT version()')
+    ## uncomment these for checking the connection
+    print('PostgreSQL database version:')
+    cursor.execute('SELECT version()')
  
-    # # display the PostgreSQL database server version
-    # db_version = cur.fetchone()
-    # print(db_version)
+    # display the PostgreSQL database server version
+    db_version = cursor.fetchone()
+    print(db_version)
        
-    # # close the communication with the PostgreSQL
-    # cur.close()
+    # close the communication with the PostgreSQL
+    cursor.close()
 except Exception as error:
     print(error)
 
 
-# @app.route("/")
-# def hello():
-#     return render_template('index.html')
+@app.route("/")
+def hello():
+    return render_template('index.html')
 
 from .views.system_admin import system_admin_bp
 app.register_blueprint(system_admin_bp)
+
--- src/bwms_app/views/system_admin.py
+++ src/bwms_app/views/system_admin.py
@@ -5,6 +5,7 @@ import psycopg2
 import json
 import datetime
 from bwms_app import cursor
+from bwms_app import conn
 
 
 system_admin_bp = Blueprint('system_admin',__name__)
@@ -13,5 +14,117 @@ system_admin_bp = Blueprint('system_admin',__name__)
 #  define the functions here and routes for system admin
 
 
-@system_admin_bp.route("/system_admin")
-def 
+@system_admin_bp.route('/system_admin/home_page')
+def home_page():
+    return render_template('system_admin/home_page.html')
+
+
+@system_admin_bp.route('/system_admin/add_roles',  methods=["POST", "GET"])
+def add_roles():
+    if request.method == "POST":
+        role_name = request.form['role_name']
+        print(role_name)
+        try:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO roles_not_associated_with_department (role_name) VALUES (%s);", (role_name,))
+            conn.commit()
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            cur.execute('rollback')
+        cur.close()
+    return render_template('system_admin/add_roles.html')    
+
+@system_admin_bp.route('/system_admin/add_department_roles',  methods=["POST", "GET"])
+def add_department_roles():
+    if request.method == "POST":
+        department_role_name = request.form['department_role_name']
+        print(department_role_name)
+        try:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO roles_associated_with_department (role_name) VALUES (%s);", (department_role_name,))
+            conn.commit()
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            cur.execute('rollback')
+        cur.close()
+    return render_template('system_admin/add_roles.html')    
+
+@system_admin_bp.route('/system_admin/show_roles',  methods=["POST", "GET"])
+def show_all_roles():
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM roles_not_associated_with_department")
+        role_data = cur.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        cur.execute('rollback')
+    cur.close()
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM roles_associated_with_department")
+        department_role_data = cur.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        cur.execute('rollback')
+    cur.close()    
+    return render_template('/system_admin/show_roles.html', role_data=role_data, department_role_data = department_role_data)        
+
+
+@system_admin_bp.route('/system_admin/add_departments',  methods=["POST", "GET"])
+def add_departments():
+    
+    if request.method == "POST":
+        department = request.form['department']
+        print(department)
+        try:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO departments (department_name, start_date) VALUES (%s, CURRENT_DATE);", (department,))
+            conn.commit()
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            cur.execute('rollback')
+        cur.close()
+    return render_template('system_admin/add_departments.html')            
+
+@system_admin_bp.route('/system_admin/show_departments',  methods=["POST", "GET"])
+def show_departments():
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM departments")
+        data = cur.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        cur.execute('rollback')
+    cur.close()
+    return render_template('/system_admin/show_departments.html', data=data)        
+
+@system_admin_bp.route('/system_admin/assign_emp_on_role',  methods=["POST", "GET"])
+def assign_emp_on_role():
+    
+    if request.method == "POST":
+        role_name = request.form['role_name']
+        post_email_id = request.form['post_email_id']
+        password = request.form['password']
+        username = request.form['username']
+        print(password)
+        try:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO employees_holding_post_not_associated_with_department (post_email_id, password, username, start_date, role_name) VALUES (%s, %s, %s, CURRENT_DATE, %s);", (post_email_id, password, username,role_name,))
+            conn.commit()
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            cur.execute('rollback')
+        cur.close()
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM roles_not_associated_with_department")
+        role_data = cur.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        cur.execute('rollback')
+    cur.close()
+    return render_template('system_admin/assign_emp_on_role.html', role_data = role_data)            
